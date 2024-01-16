d2d3bd23b6bdaa6fa010b021717c0d03ff93267c
rurumimic <10915235+rurumimic@users.noreply.github.com>
2024-01-16 22:45:12 +0900

    delete comments

--- src/design-patterns/MazeGame/maze_factory.cpp
+++ src/design-patterns/MazeGame/maze_factory.cpp
@@ -3,22 +3,3 @@
 #include <iostream>
 
 MazeFactory::MazeFactory() { std::cout << "Init MazeFactory()" << std::endl; }
-
-/*
-std::unique_ptr<Maze> MazeFactory::MakeMaze() const {
-  return std::make_unique<Maze>();
-}
-
-std::unique_ptr<Wall> MazeFactory::MakeWall() const {
-  return std::make_unique<Wall>();
-}
-
-std::unique_ptr<Room> MazeFactory::MakeRoom(int n) const {
-  return std::make_unique<Room>(n);
-}
-
-std::shared_ptr<Door> MazeFactory::MakeDoor(Room* r1, Room* r2) const {
-  return std::make_shared<Door>(r1, r2);
-}
-*/
-
